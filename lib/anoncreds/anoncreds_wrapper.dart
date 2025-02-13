46885960b9bef9400312fafa363dbc1d21ffd0bc
Bruno Brasil <bruno-brasil.ferreira@serpro.gov.br>
2025-02-13 14:33:57 -0300

    load so file and add  getAnoncredsVersion

---
+++ lib/anoncreds/anoncreds_wrapper.dart
@@ -0,0 +1,21 @@
+import 'dart:ffi';
+
+import 'package:anoncreds_wrapper_dart/anoncreds/anoncreds_native_functions.dart';
+import 'package:ffi/ffi.dart';
+
+String anoncredsVersion() {
+  Pointer<Utf8> resultPointer = nullptr;
+
+  try {
+    resultPointer = nativeAnoncredsVersion();
+    return resultPointer.toDartString();
+  } finally {
+    freePointer(resultPointer);
+  }
+}
+
+void freePointer(Pointer<NativeType> pointer) {
+  if (pointer != nullptr) {
+    calloc.free(pointer);
+  }
+}
---
+++ test/anoncreds_test.dart
@@ -0,0 +1,13 @@
+import 'package:anoncreds_wrapper_dart/anoncreds/anoncreds_wrapper.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Anoncreds Tests', () {
+    test('Get Version', () async {
+      final result = anoncredsVersion();
+      print(result);
+
+      expect(result, equals('0.2.0'));
+    });
+  });
+}
